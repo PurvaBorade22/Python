847c084a122ba4d5ad631ed718758d447d2b25f2
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-09 22:43:37 +0530

    Operator

---
+++ Operators/logicaloperator.py
@@ -0,0 +1,12 @@
+## Not operator
+a = 40
+b = 20
+print(not False) #it will print true
+print(not (a < b)) #it will return oppeoite value of the value
+
+#AND operator
+val1= True
+val2= False
+print("and operator:", val1 and val2)
+
+print("OR operator :", val1 or val2)
