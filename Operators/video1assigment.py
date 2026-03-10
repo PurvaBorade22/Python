f07ebcc323adf55d28dcf4001ccc32c98c5bfe18
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-10 17:23:51 +0530

    assignment

---
+++ Operators/video1assigment.py
@@ -0,0 +1,17 @@
+#Area of square
+side = int(input("Enter side of a square :"))
+print("area of square :", side * side)
+
+
+#Avarge of 2 numnbers
+
+a = float(input("Enter 1st no. :"))
+b = float(input("Enter 2nd no. :"))
+average = (a+b)/2
+print("Avarege of two numbers is :", average)
+
+# print true if a is greater than b ortherwise false
+
+a = float(input("Enter 1st no. :"))
+b = float(input("Enter 2nd no. :"))
+print(a>=b)
