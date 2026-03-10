5d557cd87e1a8195a5254ace39c7e55f1778ead3
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-10 16:27:29 +0530

    type conversion

--- Operators/logicaloperator.py
+++ Operators/logicaloperator.py
@@ -9,4 +9,4 @@ val1= True
 val2= False
 print("and operator:", val1 and val2)
 
-print("OR operator :", val1 or val2)
+print("OR operator :", (a ==b) or (a >b))
---
+++ Operators/typeconversion.py
@@ -0,0 +1,5 @@
+a = float("2")
+b = 4.5 
+sum = a + b
+print(type(a))
+print (sum)
