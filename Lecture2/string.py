afad9270bdb54cc7c2c0e42f72b90e36de2dfbc5
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-11 20:38:12 +0530

    Practice Q1

--- Lecture2/string.py
+++ Lecture2/string.py
@@ -1,19 +1,19 @@
-# str1= "this is python program"
-# print(str1)
+str1= "this is python program"
+print(str1)
 
-# #Comcatenation and length of string
-# str1="Helloo"
-# len1 = len(str1)
-# print(len1)
+# #Concatenation and length of string
+str1="Helloo"
+len1 = len(str1)
+print(len1)
 
-# str2="Purva"
-# len2 = len(str2)
-# print(len2)
+str2="Purva"
+len2 = len(str2)
+print(len2)
 
-# final_str = str1 + " " + str2
-# print(final_str)
-# len3 = len(final_str)
-# print(len3)
+final_str = str1 + " " + str2
+print(final_str)
+len3 = len(final_str)
+print(len3)
 
 
 #endswith function in string
---
+++ Lecture2/stringpractriceQs.py
@@ -0,0 +1,6 @@
+#Q1 length of a string
+str = input("Enter your full name: ")
+length = len(str)
+print("lenghth of your full name is: ", length)
+
+#Q2 count the character
