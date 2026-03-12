da16529567d722313ad3b89a2339894a9a8bb0e3
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-12 22:55:11 +0530

    List methods

--- Chap3-list&tuple/list.py
+++ Chap3-list&tuple/list.py
@@ -4,9 +4,31 @@
 # print(marks[3])
 
 
-student= ["purva", 22, "Airoli"]
-student[0]= "Krutika"
-print(student)
-print(student[5])
+# student= ["purva", 22, "Airoli"]
+# student[0]= "Krutika"
+# print(student)
+# print(student[5])
+
+##### list Methods ##############
+
+
+list = ["apple", "watermaelon", "banana"]
+list.append("mango")
+
+list.sort() #ascending order
+
+list.sort(reverse=True) #decending order
+
+list.reverse() #reverse list
+
+list.insert(2, "purva") #imsert element in list
+
+list.remove("apple") #remove item from the list
+
+list.pop(1) #remove element by there index
+print(list) 
+
+
+
 
 
