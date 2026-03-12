7812f257d76199a1cf1240bf2886fabda656d5f1
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-12 23:37:48 +0530

    Practice Q1 - list

---
+++ Chap3-list&tuple/pravticeQ1.py
@@ -0,0 +1,9 @@
+movie = []
+
+movie.append(input("Enter 1st movie name: "))
+movie.append(input("Enter 2nd movie name: "))
+movie.append(input("Enter 3rd movie name: "))
+
+print("Movies in list: ", movie)
+
+
