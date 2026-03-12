d0e59600fd2c205406c37792a9616947067515b1
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-12 23:16:31 +0530

    List code change

--- Chap3-list&tuple/list.py
+++ Chap3-list&tuple/list.py
@@ -1,19 +1,20 @@
-# marks = [23.9, 34.9, 77, 89, 56,9, 89.9]
-# print(marks)
-# print(type(marks))
-# print(marks[3])
+marks = [23.9, 34.9, 77, 89, 56,9, 89.9]
+print(marks)
+print(type(marks))
+print(marks[3])
 
 
-# student= ["purva", 22, "Airoli"]
-# student[0]= "Krutika"
-# print(student)
-# print(student[5])
+student= ["purva", 22, "Airoli"]
+student[0]= "Krutika"
+print(student)
+print(student[5])
 
 ##### list Methods ##############
 
 
 list = ["apple", "watermaelon", "banana"]
 list.append("mango")
+
 
 list.sort() #ascending order
 
