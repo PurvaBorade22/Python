00ea18ae57445255dd1afcb8882ffb116dbbeeeb
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-12 18:03:56 +0530

    3rd Q

--- Lec2string&conditions/video2assignment.py
+++ Lec2string&conditions/video2assignment.py
@@ -8,13 +8,18 @@
 
 ## Greatest of 2 numbers 
 
-num1 = int(input("Enter 1st numbers : "))
-num2 = int(input("Enter 2st numbers : "))
-num3 = int(input("Enter 3st numbers : "))
+# num1 = int(input("Enter 1st numbers : "))
+# num2 = int(input("Enter 2st numbers : "))
+# num3 = int(input("Enter 3st numbers : "))
 
-if(num1 > num2):
-    print("1st number is greater than 2nd number")
-elif(num2 > num3):
-    print("2nd no. is greater than 3rd no.")
-elif(num3 > num1):
-    print("3rd no. is graeter than 1st")
+# if(num1 > num2):
+#     print("1st number is greater than 2nd number")
+# elif(num2 > num3):
+#     print("2nd no. is greater than 3rd no.")
+# elif(num3 > num1):
+#     print("3rd no. is graeter than 1st")
+
+########################## divisible by 7
+
+numbers1 = int(input("Enter a number : "))
+div = number 
