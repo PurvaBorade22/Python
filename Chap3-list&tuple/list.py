16b831a585692998c4102bb6dc9b4405e1cdf38c
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-12 20:06:48 +0530

    List in python

---
+++ Chap3-list&tuple/list.py
@@ -0,0 +1,12 @@
+# marks = [23.9, 34.9, 77, 89, 56,9, 89.9]
+# print(marks)
+# print(type(marks))
+# print(marks[3])
+
+
+student= ["purva", 22, "Airoli"]
+student[0]= "Krutika"
+print(student)
+print(student[5])
+
+
