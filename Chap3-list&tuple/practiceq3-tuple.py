dc27ebd94faf465a08f75c90047b7e1fb1675c63
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-13 00:33:11 +0530

    Practice Q3

---
+++ Chap3-list&tuple/practiceq3-tuple.py
@@ -0,0 +1,10 @@
+###count no. of student with bgrade "A"
+
+tup = ("C", "B", "A","C","A","B","A")
+print(tup.count("A"))
+
+#########list a to D
+
+list1 = ["C","B", "D","C","E","F","A"]
+list1.sort()
+print(list1)
