3220433cc1e2e8c90cd30abf80d889a100a544a1
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-17 17:23:32 +0530

    Dictionary in python

---
+++ Chp4-Dict&Sets/dictionary.py
@@ -0,0 +1,13 @@
+info = {
+    "name" : "Purva",
+    "roll no" : 1234,
+    "subject" : ["Python", "C", "Java"],
+    "Topics" : ("Dict , Set"),
+    "is_adult" : True,
+    12 : "age"
+}
+info["name"]= "Purva Borade" #update name
+info["roll no"]= 4567890 #update roll no
+info["surname"]= "Borade" #we can add new dict 
+
+print(info)
