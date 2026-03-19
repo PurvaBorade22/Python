f9908a336abf79d097b0e69475dfda14ad10e526
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-19 19:19:30 +0530

    Basics of set

---
+++ Chp4-Dict&Sets/sets.py
@@ -0,0 +1,9 @@
+collection= {1, 2, 3,3,3, "Hello", "Worlds","Purva"}
+print(collection)
+print(type(collection))
+print(len(collection ))
+
+#if we want to create empty set 
+
+collection1 = set ()
+print(type(collection))
