d8df35d3f67638947ae80125daf063657a248604
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-27 16:07:50 +0530

    While loop basics

--- Chp5-Loops/while-loop.py
+++ Chp5-Loops/while-loop.py
@@ -1,4 +1,19 @@
 count = 1
-while count <= 100:
+while count <= 5:
     print("Hello", count)
     count += 1
+
+#print 1 to 5
+i = 1
+while i <= 5:
+    print(i)
+    i += 1
+print("Loop ended")
+
+
+#print 5 to 1
+i = 5
+while i >= 1:
+    print(i)
+    i -= 1
+print("Loop ended")
