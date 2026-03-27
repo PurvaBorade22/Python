8f7e85e8fc0bb4c324396c02ae832142c69cfb90
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-27 17:58:48 +0530

    while loop Q4

--- Chp5-Loops/while-loop-practiceQs.py
+++ Chp5-Loops/while-loop-practiceQs.py
@@ -18,3 +18,18 @@ i = 1
 while i <= 10:
     print(n*i)
     i += 1
+
+
+#Q4
+nums = [1,4,9,16,36,49,64,81,100]
+idx = 0
+while idx < len(nums):
+    print(nums[idx])
+    idx +=1
+ ################
+ 
+fruits =["aaple", "banana","watermelon","orange"]
+idx1 = 0
+while idx1 <len(fruits):
+    print(fruits[idx1])
+    idx1 += 1
