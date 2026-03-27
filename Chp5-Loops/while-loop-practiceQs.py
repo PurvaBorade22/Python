756a6c1d16bed1e3dbf7da102a9e63bc30770e11
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-27 17:33:39 +0530

    Practice Q1,2,3

---
+++ Chp5-Loops/while-loop-practiceQs.py
@@ -0,0 +1,20 @@
+#print 100 to 1
+i = 1                #start form
+while i <= 100:
+    print(i)
+    i += 1
+
+
+#print 100 to 1
+i = 100             #start from
+while i >= 1:
+    print(i)
+    i -= 1
+
+
+#multiplication table of 2
+n = int(input("Enter a number: "))
+i = 1
+while i <= 10:
+    print(n*i)
+    i += 1
