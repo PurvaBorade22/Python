29d775793be68fa5fb71009c1154204ce68bace8
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-29 11:17:26 +0530

    Continue loop

--- Chp5-Loops/break&Continue.py
+++ Chp5-Loops/break&Continue.py
@@ -29,3 +29,13 @@ while i <= 10:
         continue
     print(i)
     i += 1
+
+
+##print even numbers
+i = 1
+while i <= 10:
+    if(i%2 != 0):    #print even number
+        i += 1
+        continue
+    print(i)
+    i += 1
