85755282512a9e27512085b8deffd300454a1663
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-29 13:17:50 +0530

    Range function

--- Chp5-Loops/range.py
+++ Chp5-Loops/range.py
@@ -1,3 +1,14 @@
-#seq = range(10)
+seq = range(10)
 for el in range(10):
     print(el)
+
+for el in range(10): #stop 
+    print(el)
+
+for el in range(2,10): #start and stop
+    print(el)
+
+for el in range(2,10,2): #start, stop and step(increase by)
+    print(el)
+
+
