515d45a64864bfe0010e4cae57caa33405650bf4
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-29 11:51:02 +0530

    For Loop

---
+++ Chp5-Loops/For-loop.py
@@ -0,0 +1,16 @@
+# nums = [1,2,3,4,5]
+# for val in nums:
+#     print(val)
+
+# name = "Python"
+# for val in name:
+#     print(val)
+
+myname = "Purva"
+for char in myname:
+    if (char == 'r'):
+        print("found r")
+        continue
+    print(char)
+else:
+    print("END")
