1726ae29b0ec95a52a1e6c24085de1115e9d5706
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-29 20:07:31 +0530

    chp5 practice Qs

---
+++ Chp5-Loops/finalPracticeQs.py
@@ -0,0 +1,17 @@
+#Q1 sum of all numbers
+n = 6
+sum = 0 #starts with
+for i in range(1, n+1):
+    sum += i
+
+print("total sum =", sum)
+
+
+
+#Q2 factorial of a number
+n = 5
+fact = 1 #starts with   
+for i in range(1, n+1):
+    fact *= i
+
+print("factorial of a number is =", fact)
