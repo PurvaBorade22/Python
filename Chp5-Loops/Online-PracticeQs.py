7fdf89eaa9f96bd98010311dfd2e98c8e8c12bfd
PurvaBorade22 <boradepurva1@gmail.com>
2026-03-31 18:20:36 +0530

    Q1 to Q4

--- Chp5-Loops/Online-PracticeQs.py
+++ Chp5-Loops/Online-PracticeQs.py
@@ -2,5 +2,37 @@
 #     print('*' * i)
 
 
-for i in range(5,0,-1):
-    print('*' * i)
+# for i in range(5,0,-1):
+#     print('*' * i)
+
+# n=4
+# for i in range (1, n+1):
+#     print(" " * (n-i)+ "*" * (2*i-1)) 
+
+
+#Print sum of first n numbers
+# n = 7
+# sum = 0
+# for i in range(1, n+1):
+#     sum += i
+# print("Sum of all number is:", sum)
+
+#Print sum of even numbers between 1 to n
+n = 10
+sum = 0
+for i in range(2, n+1, 2):
+    sum += i
+print("sum of all even numbers is:",sum)
+
+#Print factorial of a number
+n = 5
+fact = 1
+for i in range(1, n+1):
+    fact *= i
+print("factorial of:",fact)
+
+#Print numbers from 1–50:
+# If divisible by 3 → print "Fizz"
+# If divisible by 5 → print "Buzz"
+# If both → print "FizzBuzz"
+
