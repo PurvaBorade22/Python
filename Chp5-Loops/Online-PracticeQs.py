e69b7474f643b72e9b9e43b0c7535e8c6cbad14a
PurvaBorade22 <boradepurva1@gmail.com>
2026-04-01 13:28:13 +0530

    Q4 and Q5

--- Chp5-Loops/Online-PracticeQs.py
+++ Chp5-Loops/Online-PracticeQs.py
@@ -18,21 +18,44 @@
 # print("Sum of all number is:", sum)
 
 #Print sum of even numbers between 1 to n
-n = 10
-sum = 0
-for i in range(2, n+1, 2):
-    sum += i
-print("sum of all even numbers is:",sum)
+# n = 10
+# sum = 0
+# for i in range(2, n+1, 2):
+#     sum += i
+# print("sum of all even numbers is:",sum)
 
 #Print factorial of a number
-n = 5
-fact = 1
-for i in range(1, n+1):
-    fact *= i
-print("factorial of:",fact)
+# n = 5
+# fact = 1
+# for i in range(1, n+1):
+#     fact *= i
+# print("factorial of:",fact)
 
 #Print numbers from 1–50:
 # If divisible by 3 → print "Fizz"
 # If divisible by 5 → print "Buzz"
 # If both → print "FizzBuzz"
 
+#Q4 divisiblity of 3 & 5
+# n = 50
+# for i in range(1,n+1):
+#     if (i%3 ==0 and i%5 ==0):
+#         print("FizzBuzz")
+#     elif(i%3  == 0):
+#         print("Fizz3")
+#     elif(i%5 == 0):
+#         print("Buzz5")
+#     else:
+#         print(i)
+
+#count how many even and odd 
+n= 50
+even_count = 0
+odd_count = 0
+for i in range(1,n+1):
+    if i%2 == 0:
+        even_count +=1
+    else:
+        odd_count += 1
+print("Count of even numbers is:", even_count)
+print("Count of odd number is:",odd_count)
