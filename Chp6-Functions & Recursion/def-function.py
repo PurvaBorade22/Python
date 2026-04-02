df0fd4520ad0712f38f9d60f6999fd5e6238e889
PurvaBorade22 <boradepurva1@gmail.com>
2026-04-02 21:55:21 +0530

    Average of 3 numbers

--- Chp6-Functions & Recursion/def-function.py
+++ Chp6-Functions & Recursion/def-function.py
@@ -1,6 +1,13 @@
-def cal_sum(a,b):
-    s = a+b
+# def cal_sum(a,b):
+#     s = a+b
+#     return s
+# print("sum is:", cal_sum(2,2))
+# print("sum is:", cal_sum(10,40))
+# print("sum is:", cal_sum(80,90))
+
+
+#Average of 3 numbers
+def average(a,b,c):
+    s = a+b+c/3
     return s
-print("sum is:", cal_sum(2,2))
-print("sum is:", cal_sum(10,40))
-print("sum is:", cal_sum(80,90))
+print("average of 3 number is:",average(2,3,4))
