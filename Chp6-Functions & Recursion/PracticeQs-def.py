aaf0321a41410b8f11fdf65b3977919a37b90844
PurvaBorade22 <boradepurva1@gmail.com>
2026-04-05 19:51:40 +0530

    odd and even

--- Chp6-Functions & Recursion/PracticeQs-def.py
+++ Chp6-Functions & Recursion/PracticeQs-def.py
@@ -16,9 +16,27 @@
 # print_list(cities)
 
 #Q3 find the factorial of n
-def fact_no(n):
-    fact = 1
-    for i in range(1, n+1):
-        fact = fact * i
-    return fact
-print(fact_no(3))
+# def fact_no(n):
+#     fact = 1
+#     for i in range(1, n+1):
+#         fact = fact * i
+#     return fact
+# print(fact_no(3))   
+
+#Q3 convert USD to inr
+# def USD(rupee):
+#     rate = 83
+#     INR = rupee * rate
+#     return INR
+
+# result= (USD(3))
+# print(result)
+
+#Q4 odd and even
+def odd_even(num):
+    if num % 2 == 0:
+        print(num,"Is even") 
+    else:
+        print(num,"Is odd")
+    return num
+odd_even(10)
