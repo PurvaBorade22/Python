3e45de8e3f47774b8774c75ee310870c2756177b
PurvaBorade22 <boradepurva1@gmail.com>
2026-04-05 12:26:34 +0530

    factorial of n

--- Chp6-Functions & Recursion/PracticeQs-def.py
+++ Chp6-Functions & Recursion/PracticeQs-def.py
@@ -1,14 +1,24 @@
 #Q1 length of a list
-cities = ["Mumbai","Pune","Airoli","Navi Mumbai","Thane","Mulund","CSMT"]
+# cities = ["Mumbai","Pune","Airoli","Navi Mumbai","Thane","Mulund","CSMT"]
 
-def get_len(city_list):
-    print("length of cities is:",len(city_list))
-    return city_list
-get_len(cities)
+# def get_len(city_list):
+#     print("length of cities is:",len(city_list))
+#     return city_list
+# get_len(cities)
 
-#Print list item in a single line (hint: "end function of print()")
-cities = ["Mumbai","Pune","Airoli","Navi Mumbai","Thane","Mulund","CSMT"]
+#Q2 Print list item in a single line (hint: "end function of print()")
+# cities = ["Mumbai","Pune","Airoli","Navi Mumbai","Thane","Mulund","CSMT"]
  
-def print_list(list):
-    for item in list:
-        print(item, end=" ")
+# def print_list(list):
+#     for item in list:
+#         print(item, end=" ")
+
+# print_list(cities)
+
+#Q3 find the factorial of n
+def fact_no(n):
+    fact = 1
+    for i in range(1, n+1):
+        fact = fact * i
+    return fact
+print(fact_no(3))
