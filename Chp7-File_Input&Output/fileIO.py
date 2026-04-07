5b6a1041e1637372aa5abd5ff5468b96c4f44d88
PurvaBorade22 <boradepurva1@gmail.com>
2026-04-07 12:51:23 +0530

    r+ mode

--- Chp7-File_Input&Output/fileIO.py
+++ Chp7-File_Input&Output/fileIO.py
@@ -10,7 +10,17 @@
 
 
 #Writing to a file
-f = open("D:\PURVA\Python\Python\Chp7-File_Input&Output\demo.txt","a")
-f.write("\nThis is another line")
-f.close()
+# f = open("D:\PURVA\Python\Python\Chp7-File_Input&Output\demo.txt","a")
+# f.write("\nThis is another line")
+# f.close()
 
+#Create new file directly
+# f = open("sample.txt","w")
+# f.close()
+
+#R+ mode ->overwrite
+
+f = open("D:\PURVA\Python\Python\Chp7-File_Input&Output\demo.txt","r+")
+f.write("abc")
+print(f.read())
+f.close()
