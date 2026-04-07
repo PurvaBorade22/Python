5ad7eb62893879f44db63852b60399aacfe2a081
PurvaBorade22 <boradepurva1@gmail.com>
2026-04-07 12:31:22 +0530

    Writing to a file

--- Chp7-File_Input&Output/fileIO.py
+++ Chp7-File_Input&Output/fileIO.py
@@ -1,8 +1,16 @@
-f = open("D:\PURVA\Python\Python\Chp7-File_Input&Output\demo.txt","r")
-line1 = f.readline()
-print(line1)
+#Reading a file
+# f = open("D:\PURVA\Python\Python\Chp7-File_Input&Output\demo.txt","r")
+# data = f.read()
+# print(data)
 
-line2 = f.readline()
-print(line2)
+# line2 = f.readline()
+# print(line2)
 
+# f.close()
+
+
+#Writing to a file
+f = open("D:\PURVA\Python\Python\Chp7-File_Input&Output\demo.txt","a")
+f.write("\nThis is another line")
 f.close()
+
