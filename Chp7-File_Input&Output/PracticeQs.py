038443ce5a48d8da45e04eac8a1fae116c5c2c7a
PurvaBorade22 <boradepurva1@gmail.com>
2026-04-09 14:03:19 +0530

    Q3 search for a word

--- Chp7-File_Input&Output/PracticeQs.py
+++ Chp7-File_Input&Output/PracticeQs.py
@@ -4,11 +4,24 @@
 #     f.write("using Java\nI like programming in Java")
 
 #Q2 overwrite java with python
-with open("practice.txt","r") as f:
-    data = f.read()
+# with open("practice.txt","r") as f:
+#     data = f.read()
 
-new_data= data.replace("java","Python")
-print(new_data)
+# new_data= data.replace("java","Python")
+# print(new_data)
 
-with open("practice.txt","w") as f:
-    f.write(new_data)
+# with open("practice.txt","w") as f:
+#     f.write(new_data)
+
+
+#Q3
+def check_for_word():
+    word = "learning"
+    with open("practice.txt","r") as f:
+        data = f.read()
+    if (data.find(word) != -1):
+        print("Found")
+    else:
+        print("Not found")
+    
+check_for_word()
