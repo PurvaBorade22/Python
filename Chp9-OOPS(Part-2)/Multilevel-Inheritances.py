1dae1f9ce3d294dc194f54801ab1bf5c8ce1c19c
PurvaBorade22 <boradepurva1@gmail.com>
2026-04-13 19:59:19 +0530

    Example of multilevel inheritances

--- Chp9-OOPS(Part-2)/Multilevel-Inheritances.py
+++ Chp9-OOPS(Part-2)/Multilevel-Inheritances.py
@@ -1,19 +1,42 @@
-class Car:
-    @staticmethod
-    def start():
-        print("Car started")
+# class Car:
+#     @staticmethod
+#     def start():
+#         print("Car started")
 
-    @staticmethod
-    def stop():
-        print("Car Stop")
+#     @staticmethod
+#     def stop():
+#         print("Car Stop")
 
-class toyoto(Car):
-    def __init__(self,brand):
-        self.brand = brand
+# class toyoto(Car):
+#     def __init__(self,brand):
+#         self.brand = brand
 
-class fortuner(toyoto):
-    def __init__(self,type):
-        self.type = type
+# class fortuner(toyoto):
+#     def __init__(self,type):
+#         self.type = type
 
-c1 = fortuner("Disel")
-print(c1.start())
+# c1 = fortuner("Disel")
+# print(c1.start())
+
+
+#Example 1
+
+# class Person:
+#     def __init__(self,name):
+#         self.name = name
+
+# class Student:
+#     def __init__(self,marks):
+#         self.marks = marks
+
+# class Result:
+#     if self.marks >= 40:
+#         print("Pass") 
+#     else:
+#         print("Fail")   
+
+# p1 = Person("Purva")
+
+# mark = int(input("Enter marks"))
+# Student.mark()
+
