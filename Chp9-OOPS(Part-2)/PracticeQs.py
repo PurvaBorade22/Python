2cf3133d285850db6f343b9b4ffb10bf29f92798
PurvaBorade22 <boradepurva1@gmail.com>
2026-04-14 18:43:13 +0530

    Practice Q1 Area and perimeter of circle

---
+++ Chp9-OOPS(Part-2)/PracticeQs.py
@@ -0,0 +1,21 @@
+#Q1
+# class Circle:
+#     pie = 3.14
+#     def __init__(self,radius):
+#         self.radius = radius
+
+#     @property
+#     def Area(self):
+#         return self.pie * self.radius *self.radius
+
+#     @property
+#     def perimeter(self):
+#         return 2 * self.pie * self.radius
+    
+# circle1 = Circle(5.8)
+# print("Area is: ", circle1.Area)
+# print("Perimeter is:", circle1.perimeter)
+
+
+#Q2
+
