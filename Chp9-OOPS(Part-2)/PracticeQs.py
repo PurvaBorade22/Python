19dd764f89ea03d6cc0a130b3e5281e03d8f41b0
PurvaBorade22 <boradepurva1@gmail.com>
2026-04-14 19:02:57 +0530

    Q2 single Inheritances using super() keyword

--- Chp9-OOPS(Part-2)/PracticeQs.py
+++ Chp9-OOPS(Part-2)/PracticeQs.py
@@ -18,4 +18,22 @@
 
 
 #Q2
+class Employee:
+    def __init__(self,role,dep,sal):
+        self.role = role
+        self.dep = dep
+        self.sal = sal
 
+    def showDetails(self):
+        print("Role:", self.role)
+        print("Department:", self.dep)
+        print("Salary:", self.sal)
+        
+class Engineer(Employee):
+    def __init__(self,name,age):
+        self.name = name
+        self.age = age
+        super().__init__("Software Tester","IT", "60,000")
+
+emp1 = Engineer("Purva",22)
+emp1.showDetails()
